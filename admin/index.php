814e2e92f0514baeb886ba123051dd8f4dce7ffe
nxcl1p <dimafnaf555@gmail.com>
2023-05-15 16:28:35 +0300

    ert

--- admin/index.php
+++ admin/index.php
@@ -8,10 +8,10 @@ error_reporting(-1);
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
 	button{
 		padding: 10px 20px;
 		font-size: 16px;
@@ -53,7 +53,7 @@ error_reporting(-1);
 		border-radius: 5px;
 		font-size: 16px;
 	}
-</style>
+	</style>
 <title>Admin</title>
 </head>
 
@@ -62,36 +62,36 @@ error_reporting(-1);
 <!-- Add -->
 <h3>Add product</h3>
 <form action="process.php" method="post" enctype="multipart/form-data" autocomplete="off">
-<input type="hidden" value="add" name="action">
-<label for="image">Add Image</label>
-<input type="file" name="image" id="image">
-<label for="name">Product Name</label>
-<input type="text" name="name" placeholder="Product Name" id="name">
-<label for="price">Product Price</label>
-<input type="number" name="price" placeholder="Product Price" id="price">
-<button type="submit">Submit</button>
+	<input type="hidden" value="add" name="action">
+	<label for="image">Add Image</label>
+	<input type="file" name="image" id="image">
+	<label for="name">Product Name</label>
+	<input type="text" name="name" placeholder="Product Name" id="name">
+	<label for="price">Product Price</label>
+	<input type="number" name="price" placeholder="Product Price" id="price">
+	<button type="submit">Submit</button>
 </form><br>
 
 <!-- Edit -->
 <h3>Edit product</h3>
 <form action="process.php" method="post" autocomplete = "off">
-<input type="hidden" name="action" value="edit">
-<label for="update_id">Product ID:</label>
-<input type="number" name="id" id="update_id">
-<label for="update_name">Name:</label>
-<input type="text" name="name" id="update_name">
-<label for="update_price">Price:</label>
-<input type="number" name="price" id="update_price">
-<button type="submit">Edit Product</button>
+	<input type="hidden" name="action" value="edit">
+	<label for="update_id">Product ID:</label>
+	<input type="number" name="id" id="update_id">
+	<label for="update_name">Name:</label>
+	<input type="text" name="name" id="update_name">
+	<label for="update_price">Price:</label>
+	<input type="number" name="price" id="update_price">
+	<button type="submit">Edit Product</button>
 </form><br>
 
 <!-- Delete -->
 <h3>Delete product</h3>
 <form action="process.php" method="post" autocomplete="off">
-<input type="hidden" name="action" value="delete">
-<label for="id">Product ID:</label>
-<input type="number" name="id" id="id">
-<button type="submit">Delete Product</button>
+	<input type="hidden" name="action" value="delete">
+	<label for="id">Product ID:</label>
+	<input type="number" name="id" id="id">
+	<button type="submit">Delete Product</button>
 </form>
 </div>
 </body>
