c534cd1bc1ee97bd8811926c192e34ad2c3ceba9
nxcl1p <dimafnaf555@gmail.com>
2023-05-15 16:26:13 +0300

    ahahhaha

--- admin/functions.php
+++ admin/functions.php
@@ -3,48 +3,48 @@ require_once 'db.php';
 error_reporting(-1);
 
 function processImage() {
-  $target_dir = "../uploads/";
-  $image_name = uniqid() . "_" . basename($_FILES["image"]["name"]);
-  $target_file = $target_dir . $image_name;
-  if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-    $image = $target_file;
-    return $image;
-  } else {
-    return false;
-  }
+	$target_dir = "../uploads/";
+	$image_name = uniqid() . "_" . basename($_FILES["image"]["name"]);
+	$target_file = $target_dir . $image_name;
+	if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+		$image = $target_file;
+		return $image;
+	} else {
+		return false;
+	}
 }
 
 function addProduct($image, $name, $price){
-  global $conn;
-  $stmt = $conn -> prepare("INSERT INTO products (image, name, price) VALUES (?, ?, ?)");
-  $stmt -> bind_param("ssi", $image, $name, $price);
-  $stmt -> execute();
-  $stmt -> close();
+	global $conn;
+	$stmt = $conn -> prepare("INSERT INTO products (image, name, price) VALUES (?, ?, ?)");
+	$stmt -> bind_param("ssi", $image, $name, $price);
+	$stmt -> execute();
+	$stmt -> close();
 }
 
 function editProduct($id, $name, $price){
-  global $conn;
-  $stmt = $conn->prepare("UPDATE products SET name = ?, price = ? WHERE id = ?");
-  $stmt -> bind_param("ssi", $name, $price, $id);
-  $stmt -> execute();
-  $stmt -> close();
+	global $conn;
+	$stmt = $conn->prepare("UPDATE products SET name = ?, price = ? WHERE id = ?");
+	$stmt -> bind_param("ssi", $name, $price, $id);
+	$stmt -> execute();
+	$stmt -> close();
 }
 
 function deleteProduct($id){
-  global $conn;
-  $stmt = $conn -> prepare("DELETE FROM products WHERE id = ?");
-  $stmt -> bind_param("i", $id);
-  $stmt -> execute();
-  $stmt -> close();
+	global $conn;
+	$stmt = $conn -> prepare("DELETE FROM products WHERE id = ?");
+	$stmt -> bind_param("i", $id);
+	$stmt -> execute();
+	$stmt -> close();
 }
 
 function getProducts()
 {
-  global $conn;
-  $result = $conn->query("SELECT * FROM products");
-  $products = [];
-  while ($row = $result->fetch_assoc()) {
-    $products[] = $row;
-  }
-  return $products;
+	global $conn;
+	$result = $conn->query("SELECT * FROM products");
+	$products = [];
+	while ($row = $result->fetch_assoc()) {
+		$products[] = $row;
+	}
+	return $products;
 }
--- admin/process.php
+++ admin/process.php
@@ -6,30 +6,31 @@ error_reporting(-1);
 $action = $_POST['action'];
 
 switch ($action) {
-  case 'add':
-    $image = processImage();
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    if ($image) {
-      addProduct($image, $name, $price);
-    } else {
-      echo "Ошибка при загрузке файла.";
-    }
-    break;
-  case 'edit':
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    editProduct($id, $name, $price);
-    break;
-  case 'delete':
-    $id = $_POST['id'];
-    deleteProduct($id);
-    break;
-  default:
-    die("Invalid action");
+case 'add':
+	$image = processImage();
+	$name = $_POST['name'];
+	$price = $_POST['price'];
+	if ($image) {
+		addProduct($image, $name, $price);
+	} else {
+		echo "Ошибка при загрузке файла.";
+	}
+	break;
+case 'edit':
+	$id = $_POST['id'];
+	$name = $_POST['name'];
+	$price = $_POST['price'];
+	editProduct($id, $name, $price);
+	break;
+case 'delete':
+	$id = $_POST['id'];
+	deleteProduct($id);
+	break;
+default:
+	die("Invalid action");
 }
 
 header('Location: ../index_user.php');
 exit();
+
 
